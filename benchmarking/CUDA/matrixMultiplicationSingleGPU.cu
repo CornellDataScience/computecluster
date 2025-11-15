3c6171619bcca19aca72d99459dea97f96f25bcb
Rahul Basak <rab547@cornell.edu>
2025-11-15 16:14:10 +0000

    update cuda

--- benchmarking/CUDA/matrixMultiplicationSingleGPU.cu
+++ benchmarking/CUDA/matrixMultiplicationSingleGPU.cu
@@ -13,3 +13,23 @@
 } while (0)
 
 
+__global__ void matmulNaive(const float* A, const float* B, float* C,
+                            int M, int K, int N) {
+
+    // row anc column this thread will be responsible for
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Bounds check (threads outside matrix do nothing)
+    if (row >= M || col >= N) return;
+
+    float sum = 0.0f;
+    // Dot product of row of A and column of B
+    for (int k = 0; k < K; ++k) {
+        float a = A[row * K + k];   // A[row, k]
+        float b = B[k * N + col];   // B[k, col]
+        sum += a * b;
+    }
+
+    C[row * N + col] = sum;
+}
