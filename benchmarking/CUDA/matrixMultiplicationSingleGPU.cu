c00a80a5c8197fb907321cf12c8c262b920cdbdd
Rahul Basak <rab547@cornell.edu>
2025-11-15 16:11:17 +0000

    update docker registry

--- benchmarking/CUDA/matrixMultiplicationSingleGPU.cu
+++ benchmarking/CUDA/matrixMultiplicationSingleGPU.cu
@@ -2,3 +2,14 @@
 #include <cstdlib>      
 #include <cmath>           
 #include <cuda_runtime.h>  
+
+#define CHECK_CUDA(call) do {                                      \
+    cudaError_t err = call;                                        \
+    if (err != cudaSuccess) {                                      \
+        fprintf(stderr, "CUDA error at %s:%d: %s\n",               \
+                __FILE__, __LINE__, cudaGetErrorString(err));      \
+        exit(EXIT_FAILURE);                                        \
+    }                                                              \
+} while (0)
+
+
