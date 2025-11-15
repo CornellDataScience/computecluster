8fd80d526f15b43950192b69892207bedebc5a26
Rahul Basak <rab547@cornell.edu>
2025-11-15 16:09:48 +0000

    update cuda

---
+++ benchmarking/CUDA/matrixMultiplicationSingleGPU.cu
@@ -0,0 +1,4 @@
+#include <cstdio>   
+#include <cstdlib>      
+#include <cmath>           
+#include <cuda_runtime.h>  
--- client.sh
+++ client.sh
@@ -4,8 +4,8 @@ python3 -m vllm.benchmarks.serve \
   --server-url http://localhost:8000 \
   --model meta-llama/Llama-2-7b-chat-hf \
   --dataset dummy \
-  --num-prompts 50 \
-  --request-rate 5 \
-  --max-num-prompts 50 \
+  --num-prompts 100 \
+  --request-rate 10 \
+  --max-num-prompts 100 \
   --timeout 200 \
   --output-file results.json
